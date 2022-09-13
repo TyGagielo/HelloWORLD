bff24afab80d16e1d2eb706dd5646fb1c704dbf5
Ty Gagielo <ty.gagielo@mrjaffesclass.com>
2022-09-13 12:05:32 -0700

    momment of truth

---
+++ hey/hey.java
@@ -0,0 +1,14 @@
+public class hey
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class hey
+     */
+    public hey()
+    {
+        // initialise instance variables
+        System.out.println("hey");
+    }
+}
